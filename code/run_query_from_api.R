267c027680bf7dd2522d36c0901ed0d21248868e
vindignia <vindigni.a@gmail.com>
2022-04-15 10:12:17 +0200

    after fetching data on 2022-04-14

--- code/run_query_from_api.R
+++ code/run_query_from_api.R
@@ -5,4 +5,4 @@ source('query_data_api.R')
 speed_run_database <- fetch_game_information_from_api()
 
 # write 
-write.csv(speed_run_database, '../data/XX')
+# write.csv(speed_run_database, '../data/XX')
